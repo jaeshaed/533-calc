33f6f7bbbacb8776570f85b180db7620cb0dbda7
bulochkaccaxarom <yana.semenova.2007@bk.ru>
2025-01-25 14:02:22 +0300

    сделал систему вычислений

---
+++ cv.py
@@ -0,0 +1,12 @@
+def add (x,y):
+    return x + y
+def sub(x,y):
+    return x-y
+def mult(x,y):
+    return x * y
+def div(x,y):
+    if y != 0:
+        return x / y
+    else:
+        return"ошибка: деление на ноль"   
+        

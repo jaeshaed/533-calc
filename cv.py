ed77de9a452b6f77683686b1d04779c54b54d9d1
Ткаченко Данила <qwerty54@gmail.com>
2025-01-30 12:47:05 +0300

    начал писать документацию

--- cv.py
+++ cv.py
@@ -1,9 +1,28 @@
 def add (x,y):
+    """Складываем два числа
+    
+    аргументы:
+        x(int): первое число
+        y(int): второе число
+    Возврат:
+        int: сложение x и y
+    """
     return x + y
+
 def subtract(x,y):
+    """Вычитание двух чисел
+
+    аргументы:
+        x(int): первое число
+        y(int): второе число
+    возврат:
+        int: вычитание x и y"""
     return x-y
+
 def multiply(x,y):
     return x * y
+
+
 def divide(x,y):
     if y != 0:
         return x / y

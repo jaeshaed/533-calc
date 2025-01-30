ea1b1ec956eb1250a950cfd3f9e269ff9bf13e19
rasul <podoidite-sprosite-luche>
2025-01-30 12:32:40 +0300

    Update test.py

--- test.py
+++ test.py
@@ -12,6 +12,36 @@ class TestMethods(unittest.TestCase):
         
         self.assertEqual(calc, supposedResult)
         
+    def test_calcMinus(self):
+        supposedResult = 0
+        handlers.handleDigit5()
+        handlers.handleOperatorMinus()
+        handlers.handleDigit5()
+        result = handlers.handleOperatorEquals()
+        calc = handlers.calcExpression(result)
+        
+        self.assertEqual(calc, supposedResult)
+        
+    def test_calcDivide(self):
+        supposedResult = 1
+        handlers.handleDigit5()
+        handlers.handleOperatorDivide()
+        handlers.handleDigit5()
+        result = handlers.handleOperatorEquals()
+        calc = handlers.calcExpression(result)
+        
+        self.assertEqual(calc, supposedResult)
+        
+    def test_calcMultiply(self):
+        supposedResult = 25
+        handlers.handleDigit5()
+        handlers.handleOperatorMult()
+        handlers.handleDigit5()
+        result = handlers.handleOperatorEquals()
+        calc = handlers.calcExpression(result)
+        
+        self.assertEqual(calc, supposedResult)
+        
 
 if __name__ == "__main__":
     unittest.main()

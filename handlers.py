df257b72df7243c0ed59c3d2a92dc3fb2d6afef8
rasul <podoidite-sprosite-luche>
2025-02-01 12:08:22 +0300

    yeas

--- handlers.py
+++ handlers.py
@@ -2,29 +2,9 @@ import cv
 
 calcStack = []
 
-def handleDigit1():
-    calcStack.append(1)
-def handleDigit2():
-    calcStack.append(2)
-def handleDigit3():
-    calcStack.append(3)
-def handleDigit4():
-    calcStack.append(4)
-def handleDigit5():
-    calcStack.append(5)
-def handleDigit6():
-    calcStack.append(6)
-def handleDigit7():
-    calcStack.append(7)
-def handleDigit8():
-    calcStack.append(8)
-def handleDigit9():
-    calcStack.append(9)
-def handleDigit0():
-    calcStack.append(0)
-    
 def HandleDigits(digit):
     calcStack.append(digit)
+    return makeToString
     
 def handleOperatorPlus():
     calcStack.append('+')
@@ -35,7 +15,8 @@ def handleOperatorMult():
 def handleOperatorDivide():
     calcStack.append('/')
 def handleOperatorEquals():
-    return calcStack
+    result = calcExpression(calcStack)
+    return result
 
 def calcExpression(expression):
     if not isinstance(expression, list):
@@ -50,10 +31,7 @@ def calcExpression(expression):
             numbers.append(item)
         elif isinstance(item, str) and item in '+-*/':
             operators.append(item)
-            
-    # print(numbers)
-    # print(operators)
-    
+          
     result = numbers[0]
     for i, operator in enumerate(operators):
         nextNumber = numbers[i+1]
@@ -65,9 +43,9 @@ def calcExpression(expression):
             finalResult = cv.multiply(result, nextNumber)
         elif operator == '/':
             finalResult = cv.divide(result, nextNumber)
-            
-    # print(result)
-    # print(nextNumber)
-    
+         
     return finalResult
         
+        
+def makeToString():
+    joined = "".join(map(str, calcStack))

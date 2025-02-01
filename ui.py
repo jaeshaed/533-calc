6c4236dad6a920f94f1574bae5faf3c1cff96df2
Арсений Вика <asukaa_a@mail.ru>
2025-02-01 12:16:35 +0300

    добавил функ

--- ui.py
+++ ui.py
@@ -33,22 +33,71 @@ photo_sbros = PhotoImage(file=r"C:\Users\Арсений\Desktop\533-calc\grafika
 
 label = ttk.Label(background="#FFFFFF", text="MUhahah", font="20")
 
-btn1 = Button(root, image=photo1 ,text=" 1 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleDigit1)
-btn2 = Button(root, image=photo2 ,text=" 2 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleDigit2)
-btn3 = Button(root, image=photo3 ,text=" 3 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleDigit3)
-btn4 = Button(root, image=photo4 ,text=" 4 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleDigit4)
-btn5 = Button(root, image=photo5 ,text=" 5 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleDigit5)
-btn6 = Button(root, image=photo6 ,text=" 6 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleDigit6)
-btn7 = Button(root, image=photo7 ,text=" 7 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleDigit7)
-btn8 = Button(root, image=photo8 ,text=" 8 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleDigit8)
-btn9 = Button(root, image=photo9 ,text=" 9 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleDigit9)
-btn0 = Button(root, image=photo0 ,text=" 0 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleDigit0)
-btn_plus = Button(root, image=photo_plus ,text=" + ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleOperatorPlus)
-btn_minus = Button(root, image=photo_minus ,text=" - ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleOperatorMinus)
-btn_umnoj = Button(root, image=photo_umnoj ,text=" * ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleOperatorMult)
-btn_delen = Button(root, image=photo_delen ,text=" / ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleOperatorDivide)
-btn_ravno = Button(root, image=photo_ravno ,text=" = ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleOperatorEquals)
-btn_sbros = Button(root, image=photo_sbros ,text=" C ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=handlers.handleErase)
+def Handler1():
+    label.config(text=handlers.handleDigit1())
+
+def Handler2():
+    label.config(text=handlers.handleDigit2())
+
+def Handler3():
+    label.config(text=handlers.handleDigit3())
+
+def Handler4():
+    label.config(text=handlers.handleDigit4())
+
+def Handler5():
+    label.config(text=handlers.handleDigit5())
+
+def Handler6():
+    label.config(text=handlers.handleDigit6())
+
+def Handler7():
+    label.config(text=handlers.handleDigit7())
+
+def Handler8():
+    label.config(text=handlers.handleDigit8())
+
+def Handler9():
+    label.config(text=handlers.handleDigit9())
+
+def Handler0():
+    label.config(text=handlers.handleDigit0())
+
+def HandlerPlus():
+    label.config(text=handlers.handleOperatorPlus())
+
+def HandlerMinus():
+    label.config(text=handlers.handleOperatorMinus())
+
+def HandlerUmnoj():
+    label.config(text=handlers.handleOperatorMult())
+
+def HandlerRavno():
+    label.config(text=handlers.handleOperatorEquals())
+
+def HandlerDelen():
+    label.config(text=handlers.handleOperatorDivide())
+    
+def HandlerClear():
+    label.config(text=handlers.handleErase())
+
+
+btn1 = Button(root, image=photo1 ,text=" 1 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=Handler1)
+btn2 = Button(root, image=photo2 ,text=" 2 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=Handler2)
+btn3 = Button(root, image=photo3 ,text=" 3 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=Handler3)
+btn4 = Button(root, image=photo4 ,text=" 4 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=Handler4)
+btn5 = Button(root, image=photo5 ,text=" 5 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=Handler5)
+btn6 = Button(root, image=photo6 ,text=" 6 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=Handler6)
+btn7 = Button(root, image=photo7 ,text=" 7 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=Handler7)
+btn8 = Button(root, image=photo8 ,text=" 8 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=Handler8)
+btn9 = Button(root, image=photo9 ,text=" 9 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=Handler9)
+btn0 = Button(root, image=photo0 ,text=" 0 ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=Handler0)
+btn_plus = Button(root, image=photo_plus ,text=" + ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=HandlerPlus)
+btn_minus = Button(root, image=photo_minus ,text=" - ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=HandlerMinus)
+btn_umnoj = Button(root, image=photo_umnoj ,text=" * ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=HandlerUmnoj)
+btn_delen = Button(root, image=photo_delen ,text=" / ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=HandlerDelen)
+btn_ravno = Button(root, image=photo_ravno ,text=" = ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=HandlerRavno)
+btn_sbros = Button(root, image=photo_sbros ,text=" C ", cursor="hand2", borderwidth=0, background="#FFFFFF", activebackground="#FFFFFF", command=HandlerClear)
 
 label.place(x=20, y=0, width=100, height=100)
 

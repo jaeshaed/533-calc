20020740f4b34bfc1deb56dfdf5c918a51c57c88
jaeshaed <36546964+jaeshaed@users.noreply.github.com>
2025-02-01 23:30:52 +0300

    Обновил и исправил тесты.

--- test/test_handlers.py
+++ test/test_handlers.py
@@ -2,45 +2,45 @@ import unittest
 import handlers
 
 class TestMethods(unittest.TestCase):
+
+    def tearDown(self):
+        handlers.handleErase()
+
     def test_calcAddition(self):
-        supposedResult = 10
+        supposedResult = '10'
         handlers.handleDigit5()
         handlers.handleOperatorPlus()
         handlers.handleDigit5()
         result = handlers.handleOperatorEquals()
-        calc = handlers.calcExpression(result)
         
-        self.assertEqual(calc, supposedResult)
+        self.assertEqual(result, supposedResult)
         
     def test_calcMinus(self):
-        supposedResult = 0
+        supposedResult = '0'
         handlers.handleDigit5()
         handlers.handleOperatorMinus()
         handlers.handleDigit5()
         result = handlers.handleOperatorEquals()
-        calc = handlers.calcExpression(result)
         
-        self.assertEqual(calc, supposedResult)
+        self.assertEqual(result, supposedResult)
         
     def test_calcDivide(self):
-        supposedResult = 1
+        supposedResult = '1'
         handlers.handleDigit5()
         handlers.handleOperatorDivide()
         handlers.handleDigit5()
         result = handlers.handleOperatorEquals()
-        calc = handlers.calcExpression(result)
         
-        self.assertEqual(calc, supposedResult)
+        self.assertEqual(result, supposedResult)
         
     def test_calcMultiply(self):
-        supposedResult = 25
+        supposedResult = '25'
         handlers.handleDigit5()
         handlers.handleOperatorMult()
         handlers.handleDigit5()
         result = handlers.handleOperatorEquals()
-        calc = handlers.calcExpression(result)
         
-        self.assertEqual(calc, supposedResult)
+        self.assertEqual(result, supposedResult)
         
 
 if __name__ == "__main__":
